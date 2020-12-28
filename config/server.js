092d04ebdc856f7ef15eeee4a725be573109d943
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2020-12-28 17:22:18 -0300

    strapi deploy#6

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT' || 1337),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b6ef1ebc294ae17a95451cd5b360b2fb'),
