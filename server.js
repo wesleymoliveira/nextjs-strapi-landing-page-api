07e42f5a29790751fd529f0ed7d0332df62779d5
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2020-12-28 09:59:14 -0300

    strapi deploy#2

---
+++ server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT' || 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b6ef1ebc294ae17a95451cd5b360b2fb'),
+    },
+  },
+})
